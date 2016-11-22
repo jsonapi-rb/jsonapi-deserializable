a671fc921375932873a4ead9512c86a42d7423c8
Lucas Hosseini <lucas.hosseini@gmail.com>
2016-11-22 21:53:25 +0100

    Add tests for overriding default deserialization schemes.

--- spec/resource/DSL/attribute_spec.rb
+++ spec/resource/DSL/attribute_spec.rb
@@ -1,55 +1,80 @@
 require 'spec_helper'
 
 describe JSONAPI::Deserializable::Resource, '.attribute' do
-  it 'creates corresponding field if attribute is present' do
-    payload = {
-      'data' => {
-        'type' => 'foo',
-        'attributes' => { 'foo' => 'bar' }
+  context 'when providing the attribute name' do
+    it 'creates corresponding field if attribute is present' do
+      payload = {
+        'data' => {
+          'type' => 'foo',
+          'attributes' => { 'foo' => 'bar' }
+        }
       }
-    }
-    klass = Class.new(JSONAPI::Deserializable::Resource) do
-      attribute(:foo) { |foo| field foo: foo }
+      klass = Class.new(JSONAPI::Deserializable::Resource) do
+        attribute(:foo) { |foo| field foo: foo }
+      end
+      actual = klass.call(payload)
+      expected = { foo: 'bar', type: 'foo' }
+
+      expect(actual).to eq(expected)
     end
-    actual = klass.call(payload)
-    expected = { foo: 'bar', type: 'foo' }
 
-    expect(actual).to eq(expected)
-  end
+    it 'does not create corresponding field if attribute is absent' do
+      payload = { 'data' => { 'type' => 'foo' }, 'attributes' => {} }
+      klass = Class.new(JSONAPI::Deserializable::Resource) do
+        attribute(:foo) { |foo| field foo: foo }
+      end
+      actual = klass.call(payload)
+      expected = { type: 'foo' }
 
-  it 'does not create corresponding field if attribute is absent' do
-    payload = { 'data' => { 'type' => 'foo' }, 'attributes' => {} }
-    klass = Class.new(JSONAPI::Deserializable::Resource) do
-      attribute(:foo) { |foo| field foo: foo }
+      expect(actual).to eq(expected)
     end
-    actual = klass.call(payload)
-    expected = { type: 'foo' }
 
-    expect(actual).to eq(expected)
-  end
+    it 'does not create corresponding field if no attribute specified' do
+      payload = { 'data' => { 'type' => 'foo' } }
+      klass = Class.new(JSONAPI::Deserializable::Resource) do
+        attribute(:foo) { |foo| field foo: foo }
+      end
+      actual = klass.call(payload)
+      expected = { type: 'foo' }
 
-  it 'does not create corresponding field if no attribute specified' do
-    payload = { 'data' => { 'type' => 'foo' } }
-    klass = Class.new(JSONAPI::Deserializable::Resource) do
-      attribute(:foo) { |foo| field foo: foo }
+      expect(actual).to eq(expected)
     end
-    actual = klass.call(payload)
-    expected = { type: 'foo' }
 
-    expect(actual).to eq(expected)
-  end
-
-  it 'defaults to creating a field with same name' do
-    payload = {
-      'data' => {
-        'type' => 'foo',
-        'attributes' => { 'foo' => 'bar' }
+    it 'defaults to creating a field with same name' do
+      payload = {
+        'data' => {
+          'type' => 'foo',
+          'attributes' => { 'foo' => 'bar' }
+        }
       }
-    }
-    klass = Class.new(JSONAPI::Deserializable::Resource)
-    actual = klass.call(payload)
-    expected = { foo: 'bar', type: 'foo' }
+      klass = Class.new(JSONAPI::Deserializable::Resource)
+      actual = klass.call(payload)
+      expected = { foo: 'bar', type: 'foo' }
 
-    expect(actual).to eq(expected)
+      expect(actual).to eq(expected)
+    end
+  end
+
+  context 'when omitting the attribute name' do
+    it 'sets the default attribute deserialization scheme' do
+      payload = {
+        'data' => {
+          'type' => 'foo',
+          'attributes' => {
+            'foo' => 'bar',
+            'baz' => 'foo'
+          }
+        }
+      }
+      klass = Class.new(JSONAPI::Deserializable::Resource) do
+        attribute do |name, value|
+          field "custom_#{name}".to_sym => value
+        end
+      end
+      actual = klass.call(payload)
+      expected = { custom_foo: 'bar', custom_baz: 'foo', type: 'foo' }
+
+      expect(actual).to eq(expected)
+    end
   end
 end
--- spec/resource/DSL/has_many_spec.rb
+++ spec/resource/DSL/has_many_spec.rb
@@ -11,86 +11,120 @@ describe JSONAPI::Deserializable::Resource, '.has_many' do
     end
   end
 
-  context 'relationship is not empty' do
-    let(:payload) do
-      {
+  context 'when providing the relationship name' do
+    context 'relationship is not empty' do
+      let(:payload) do
+        {
+          'data' => {
+            'type' => 'foo',
+            'relationships' => {
+              'foo' => {
+                'data' => [
+                  { 'type' => 'foo', 'id' => 'bar' },
+                  { 'type' => 'foo', 'id' => 'baz' }
+                ]
+              }
+            }
+          }
+        }
+      end
+
+      it 'creates corresponding fields' do
+        actual = deserializable_foo.call(payload)
+        expected = { foo_ids: %w(bar baz), foo_types: %w(foo foo),
+                     foo_rel: payload['data']['relationships']['foo'],
+                     type: 'foo' }
+
+        expect(actual).to eq(expected)
+      end
+
+      it 'defaults to creating a #{name}_ids and #{name}_types fields' do
+        klass = Class.new(JSONAPI::Deserializable::Resource)
+        actual = klass.call(payload)
+        expected = { foo_ids: %w(bar baz), foo_types: %w(foo foo), type: 'foo' }
+
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context 'relationship is empty' do
+      it 'creates corresponding fields' do
+        payload = {
+          'data' => {
+            'type' => 'foo',
+            'relationships' => {
+              'foo' => {
+                'data' => []
+              }
+            }
+          }
+        }
+        actual = deserializable_foo.call(payload)
+        expected = { foo_ids: [], foo_types: [],
+                     foo_rel: payload['data']['relationships']['foo'],
+                     type: 'foo' }
+
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context 'relationship is absent' do
+      it 'does not create corresponding fields' do
+        payload = {
+          'data' => {
+            'type' => 'foo',
+            'relationships' => {}
+          }
+        }
+        actual = deserializable_foo.call(payload)
+        expected = { type: 'foo' }
+
+        expect(actual).to eq(expected)
+      end
+    end
+
+    context 'there is no relationships member' do
+      it 'does not create corresponding fields' do
+        payload = {
+          'data' => {
+            'type' => 'foo'
+          }
+        }
+        actual = deserializable_foo.call(payload)
+        expected = { type: 'foo' }
+
+        expect(actual).to eq(expected)
+      end
+    end
+  end
+
+  context 'when omitting the relationship name' do
+    it 'sets the default has_many relationship deserialization scheme' do
+      payload = {
         'data' => {
           'type' => 'foo',
           'relationships' => {
             'foo' => {
-              'data' => [
-                { 'type' => 'foo', 'id' => 'bar' },
-                { 'type' => 'foo', 'id' => 'baz' }
-              ]
+              'data' => [{ 'type' => 'bar', 'id' => 'baz' },
+                         { 'type' => 'foo', 'id' => 'bar' }]
+            },
+            'bar' => {
+              'data' => [{ 'type' => 'baz', 'id' => 'foo' },
+                         { 'type' => 'baz', 'id' => 'buz' }]
             }
           }
         }
       }
-    end
-
-    it 'creates corresponding fields' do
-      actual = deserializable_foo.call(payload)
-      expected = { foo_ids: %w(bar baz), foo_types: %w(foo foo),
-                   foo_rel: payload['data']['relationships']['foo'],
-                   type: 'foo' }
-
-      expect(actual).to eq(expected)
-    end
-
-    it 'defaults to creating a #{name}_ids and #{name}_types fields' do
-      klass = Class.new(JSONAPI::Deserializable::Resource)
+      klass = Class.new(JSONAPI::Deserializable::Resource) do
+        has_many do |name, _value, ids, types|
+          field "custom_#{name}_ids".to_sym => ids
+          field "custom_#{name}_types".to_sym => types
+        end
+      end
       actual = klass.call(payload)
-      expected = { foo_ids: %w(bar baz), foo_types: %w(foo foo), type: 'foo' }
-
-      expect(actual).to eq(expected)
-    end
-  end
-
-  context 'relationship is empty' do
-    it 'creates corresponding fields' do
-      payload = {
-        'data' => {
-          'type' => 'foo',
-          'relationships' => {
-            'foo' => {
-              'data' => []
-            }
-          }
-        }
-      }
-      actual = deserializable_foo.call(payload)
-      expected = { foo_ids: [], foo_types: [],
-                   foo_rel: payload['data']['relationships']['foo'],
+      expected = { custom_foo_ids: %w(baz bar), custom_foo_types: %w(bar foo),
+                   custom_bar_ids: %w(foo buz), custom_bar_types: %w(baz baz),
                    type: 'foo' }
-
-      expect(actual).to eq(expected)
-    end
-  end
-
-  context 'relationship is absent' do
-    it 'does not create corresponding fields' do
-      payload = {
-        'data' => {
-          'type' => 'foo',
-          'relationships' => {}
-        }
-      }
-      actual = deserializable_foo.call(payload)
-      expected = { type: 'foo' }
-
-      expect(actual).to eq(expected)
-    end
-  end
-
-  context 'there is no relationships member' do
-    it 'does not create corresponding fields' do
-      payload = {
-        'data' => {
-          'type' => 'foo'
-        }
-      }
-      actual = deserializable_foo.call(payload)
-      expected = { type: 'foo' }
 
       expect(actual).to eq(expected)
     end
